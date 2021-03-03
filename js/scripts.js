76316633eca04be47693542f13a49e7b9a16a051
Christine Clark <chrstn.clark@gmail.com>
2021-03-03 14:24:02 -0700

    working quote gallery

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,63 @@
 // '"Creativity takes courage" - Henry Matisse'
 // ]
 
-var quote1 = {
-	quote: '"We don\'t make mistakes, just happy little accidents."',
-	author: "Bob Ross",
-	tags: ["painting", "mistakes"],
-	color: "#0a3410",
-	display: function() {
+function Quote(quoteText, author, tags, color) {
+	this.quote = quoteText;
+	this.author = author;
+	this.tags = tags;
+	this.color = color;
+	this.display = function() {
+
+		var container = $("<div>")
+		this.tags.forEach(function(tag){
+			container.addClass(tag);
+		})
+		container.css("background", this.color)
+		container.addClass("quote")
+
 		var quoteString = "";
-		quoteString += "<div style='background:" + this.color + "'>";
 		quoteString += "<p>" + this.quote + "</p>";
 		quoteString += "<cite>" + this.author + "</cite";
-		quoteString += "</div>"
-		$("body").prepend(quoteString)
+
+		container.html(quoteString)
+		$(".quotes").prepend(container)
 	}
 }
 
-quote1.display();
+var quotes = [
+	new Quote('"We don\'t make mistakes, just happy little accidents."', "Bob Ross", ["painting", "mistakes"], "#0a3410"),
+	new Quote('"Creativity takes courage"', "Henry Matisse", ["painting", "creativity"], "lightblue"),
+	new Quote('"this is another one"', "Christine", ["tag1"], "violet")
+]
 
-console.log(quote1.quote)
+//global taglist
+var tagList = []
 
-// var quoteString = "";
-// quotes.forEach(function(quote){
+quotes.forEach(function(quote){
+	quote.display();
+	quote.tags.forEach(function(tag){
+		// check to see if tag has been added to taglist
+		if(!tagList.includes(tag)) {
+			// if it isnt added, add it
+			tagList.push(tag);
+			// and also make a button for it.
+			$(".buttons").prepend("<button class='filter' id='" + tag + "'>" + tag + "</button>")
+		}
+	})
+})
+console.log(tagList)
+
+$(".filter").on("click", function() {
+	var tag = $(this).attr("id");
+	console.log(tag)
 	
-// 	quoteString += "<p>" + quote + "</p>"
+	$(".quote").not("." + tag).hide();
+	$("." + tag).fadeIn();
 
-// 	console.log(quoteString);
-// })
+	$(".filter").removeClass("active")
+	$(this).addClass("active")
 
-// $("body").html(quoteString)
+})
+
+
+
